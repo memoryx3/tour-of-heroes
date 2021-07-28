32f4ac4cf0e0af3784560f70054ca7e4d3f63066
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-07-28 21:52:13 +0300

    Import and config HeroSevice to HeroComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -8,16 +8,23 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  readonly heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService
+  ) { }
 
   onSelect(selectedHero: Hero) {
     this.selectedHero = selectedHero;
   }
 
+  getHeroes() {
+    this.heroes = this.heroService.getHeroes();
+  }
+
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
