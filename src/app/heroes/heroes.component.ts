6615831fcb859d15818817c2abdfb5ea30fc8c97
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-07-28 19:08:59 +0300

    Make dashboard of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,10 +8,7 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  readonly hero: Hero = {
-    id: 1,
-    name: `Wildstorm`
-  } 
+  readonly heroes = HEROES;
 
   constructor() { }
 
