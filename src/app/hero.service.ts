82da9021e5a358fed68b0379cea9f823a8f2a35e
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-07-30 04:59:35 +0300

    Refactor getGeroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,23 +3,33 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes'; 
 
-getHeroes(): Observable<Hero[]> {
-  const heroes = of(HEROES);
-  this.messageService.add('HeroService: fetched heroes');
-  return heroes;
-}
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
 
-getHero(id: number): Observable<Hero> {
-  const hero = HEROES.find(h => h.id === id)!;
-  this.messageService.add(`HeroService: fetched hero id=${id}`);
-  return of(hero)
-}
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero)
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  
 }
